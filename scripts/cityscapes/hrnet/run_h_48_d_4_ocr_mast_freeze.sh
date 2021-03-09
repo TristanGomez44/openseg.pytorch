5e2d0ce6cbef85ea2129806feaed255dc2c87b3e
TristanGomez44 <tristan.gomez@etu.univ-nantes.fr>
2021-03-09 11:25:28 +0100

    Increased max batch size

---
+++ scripts/cityscapes/hrnet/run_h_48_d_4_ocr_mast_freeze.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+cd $SCRIPTPATH
+cd ../../../
+. config.profile
+# check the enviroment info
+
+export PYTHONPATH="$PWD":$PYTHONPATH
+
+DATA_ROOT="./datasets/"
+DATA_DIR="${DATA_ROOT}/cityscapes"
+SAVE_DIR="${DATA_ROOT}/seg_result/cityscapes/"
+BACKBONE="hrnet48"
+
+CONFIGS="configs/cityscapes/H_48_D_4.json"
+CONFIGS_TEST="configs/cityscapes/H_48_D_4_TEST.json"
+
+MODEL_NAME="hrnet_w48_ocr"
+LOSS_TYPE="fs_auxce_loss"
+CHECKPOINTS_NAME="${MODEL_NAME}_teach_freeze_"$2
+LOG_FILE="./log/cityscapes/${CHECKPOINTS_NAME}.log"
+echo "Logging to $LOG_FILE"
+mkdir -p `dirname $LOG_FILE`
+
+PRETRAINED_MODEL="./pretrained_model/hrnet_w48_ocr_1_latest.pth"
+MAX_ITERS=1000
+BATCHSIZE=8
+
+
+if [ "$1"x == "train"x ]; then
+  python3 main.py --configs ${CONFIGS} \
+                       --drop_last y \
+                       --phase train \
+                       --gathered n \
+                       --loss_balance y \
+                       --log_to_file n \
+                       --backbone ${BACKBONE} \
+                       --model_name ${MODEL_NAME} \
+                       --gpu 0 1 2 3 \
+                       --data_dir ${DATA_DIR} \
+                       --loss_type ${LOSS_TYPE} \
+                       --max_iters ${MAX_ITERS} \
+                       --checkpoints_name ${CHECKPOINTS_NAME} \
+                       --resume  ${PRETRAINED_MODEL} \
+                       --exp_id cityscapes \
+                       --use_teach True \
+                       --max_batch_size 24 \
+                       --freeze_feature True \
+                       2>&1 | tee ${LOG_FILE}
+
+
+elif [ "$1"x == "resume"x ]; then
+  ${PYTHON} -u main.py --configs ${CONFIGS} \
+                       --drop_last y \
+                       --phase train \
+                       --gathered n \
+                       --loss_balance y \
+                       --log_to_file n \
+                       --backbone ${BACKBONE} \
+                       --model_name ${MODEL_NAME} \
+                       --max_iters ${MAX_ITERS} \
+                       --data_dir ${DATA_DIR} \
+                       --loss_type ${LOSS_TYPE} \
+                       --gpu 0 1 2 3 \
+                       --resume_continue y \
+                       --resume ./checkpoints/cityscapes/${CHECKPOINTS_NAME}_latest.pth \
+                       --checkpoints_name ${CHECKPOINTS_NAME} \
+                        2>&1 | tee -a ${LOG_FILE}
+
+
+elif [ "$1"x == "val"x ]; then
+  ${PYTHON} -u main.py --configs ${CONFIGS} --drop_last y --data_dir ${DATA_DIR} \
+                       --backbone ${BACKBONE} --model_name ${MODEL_NAME} --checkpoints_name ${CHECKPOINTS_NAME} \
+                       --phase test --gpu 0 1 2 3 --resume ./checkpoints/cityscapes/${CHECKPOINTS_NAME}_latest.pth \
+                       --loss_type ${LOSS_TYPE} --test_dir ${DATA_DIR}/val/image \
+                       --out_dir ${SAVE_DIR}${CHECKPOINTS_NAME}_val
+
+  cd lib/metrics
+  ${PYTHON} -u cityscapes_evaluator.py --pred_dir ${SAVE_DIR}${CHECKPOINTS_NAME}_val/label  \
+                                       --gt_dir ${DATA_DIR}/val/label
+
+elif [ "$1"x == "segfix"x ]; then
+  if [ "$3"x == "test"x ]; then
+    DIR=${SAVE_DIR}${CHECKPOINTS_NAME}_test_ss/label
+    echo "Applying SegFix for $DIR"
+    ${PYTHON} scripts/cityscapes/segfix.py \
+      --input $DIR \
+      --split test \
+      --offset ${DATA_ROOT}/cityscapes/test_offset/semantic/offset_hrnext/
+  elif [ "$3"x == "val"x ]; then
+    DIR=${SAVE_DIR}${CHECKPOINTS_NAME}_val/label
+    echo "Applying SegFix for $DIR"
+    ${PYTHON} scripts/cityscapes/segfix.py \
+      --input $DIR \
+      --split val \
+      --offset ${DATA_ROOT}/cityscapes/val/offset_pred/semantic/offset_hrnext/
+  fi
+
+elif [ "$1"x == "test"x ]; then
+  if [ "$3"x == "ss"x ]; then
+    echo "[single scale] test"
+    ${PYTHON} -u main.py --configs ${CONFIGS} --drop_last y \
+                         --backbone ${BACKBONE} --model_name ${MODEL_NAME} --checkpoints_name ${CHECKPOINTS_NAME} \
+                         --phase test --gpu 0 1 2 3 --resume ./checkpoints/cityscapes/${CHECKPOINTS_NAME}_latest.pth \
+                         --test_dir ${DATA_DIR}/test --log_to_file n \
+                         --out_dir ${SAVE_DIR}${CHECKPOINTS_NAME}_test_ss
+  else
+    echo "[multiple scale + flip] test"
+    ${PYTHON} -u main.py --configs ${CONFIGS_TEST} --drop_last y \
+                         --backbone ${BACKBONE} --model_name ${MODEL_NAME} --checkpoints_name ${CHECKPOINTS_NAME} \
+                         --phase test --gpu 0 1 2 3 --resume ./checkpoints/cityscapes/${CHECKPOINTS_NAME}_latest.pth \
+                         --test_dir ${DATA_DIR}/test --log_to_file n \
+                         --out_dir ${SAVE_DIR}${CHECKPOINTS_NAME}_test_ms
+  fi
+
+
+else
+  echo "$1"x" is invalid..."
+fi
